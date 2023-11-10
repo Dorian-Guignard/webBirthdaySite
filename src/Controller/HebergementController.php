d137003b839e4e7171d5d197e29e86fb9e16703e
Dorian-Guignard <web.guidor@gmail.com>
2023-11-10 11:15:08 +0100

    hebergement

---
+++ src/Controller/HebergementController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Services\AuthenticationService;
+
+class HebergementController extends AbstractController
+{
+    /**
+     * @Route("/hebergement", name="app_hebergement")
+     */
+    public function index(AuthenticationService $authenticationService): Response
+    {
+
+        $isAuthenticated = $authenticationService->isAuthenticated();
+
+        return $this->render('hebergement/index.html.twig', [
+            'controller_name' => 'HebergementController',
+            'isAuthenticated' => $isAuthenticated,
+        ]);
+
+    }
+}
