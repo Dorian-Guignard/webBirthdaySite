ff90f1a328d8c52eea8ab07424bf572d95d5c296
Dorian-Guignard <web.guidor@gmail.com>
2023-11-20 11:19:21 +0100

    reboursFalse

--- public/js/app.js
+++ public/js/app.js
@@ -6,41 +6,45 @@ var iconContainer = document.getElementById("confetti-icon");
     document.querySelector('.modal').style.display = 'none';
   } 
 
-function countdown() {
-  
-  // Définir les dates de début et de fin
-  const dateDebut = new Date('1974-06-29T08:00:00');
-  const dateFin = new Date('2024-06-29T08:00:00');
+document.addEventListener('DOMContentLoaded', function (event) {
+  event.preventDefault();
+  // Initialiser le compte à rebours
+  countdown();
+});
 
-  // Calculer la différence entre les deux dates
+function countdown() {
+ 
+  // Définir les dates de début et de fin
+  const dateDebut = new Date('1974-06-29T08:00:00').getTime();
+  const dateFin = new Date('2024-06-29T08:00:00').getTime();
+
+  // Calculer la différence entre les deux dates en millisecondes
   const difference = dateFin - dateDebut;
 
   // Convertir la différence en années, mois, jours, heures, minutes et secondes
-  const years = difference.getUTCFullYear() - 1974;
-  const months = difference.getUTCMonth() + 1;
-  const days = difference.getUTCDate();
-  const hours = difference.getUTCHours();
-  const minutes = difference.getUTCMinutes();
-  const seconds = difference.getUTCSeconds();
+  const years = new Date(difference).getUTCFullYear() - 1970;
+  const months = new Date(difference).getUTCMonth();
+  const days = new Date(difference).getUTCDate() - 1;
+  const hours = new Date(difference).getUTCHours();
+  const minutes = new Date(difference).getUTCMinutes();
+  const seconds = new Date(difference).getUTCSeconds();
 
-console.log('Years:', years);
-console.log('Months:', months);
-console.log('Days:', days);
-console.log('Hours:', hours);
-console.log('Minutes:', minutes);
-console.log('Seconds:', seconds);
+  console.log('Years:', years);
+  console.log('Months:', months);
+  console.log('Days:', days);
+  console.log('Hours:', hours);
+  console.log('Minutes:', minutes);
+  console.log('Seconds:', seconds);
 
   // Afficher le compte à rebours
-    document.getElementById('years').innerHTML = `Années : ${years}`;
-    document.getElementById('months').innerHTML = `Mois : ${months}`;
-    document.getElementById('days').innerHTML = `Jours : ${days}`;
-    document.getElementById('hours').innerHTML = `H : ${hours}`;
-    document.getElementById('minutes').innerHTML = `M : ${minutes}`;
-    document.getElementById('seconds').innerHTML = `S : ${seconds}`;
+  document.getElementById('years').innerHTML = `Années : ${years}`;
+  document.getElementById('months').innerHTML = `Mois : ${months}`;
+  document.getElementById('days').innerHTML = `Jours : ${days}`;
+  document.getElementById('hours').innerHTML = `H : ${hours}`;
+  document.getElementById('minutes').innerHTML = `M : ${minutes}`;
+  document.getElementById('seconds').innerHTML = `S : ${seconds}`;
 
   // Répéter le compte à rebours toutes les secondes
   setTimeout(countdown, 1000);
 }
 
-// Initialiser le compte à rebours
-countdown();
